12d77f0b69c97392b7d8f6cf11c8d014aedde169
simsonpeter <simsonpeter@gmail.com>
2025-01-30 03:27:12 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+﻿const CACHE_NAME = 'tc-radios-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+
+// Install the service worker and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+
+// Fetch cached assets or make a network request
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
